964920163c67044156ec64c3788b6e1df830c88a
Arvind.H <arvind.hasti@gmail.com>
2017-08-14 23:00:37 +0530

    Added remember_digest to users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813091208) do
+ActiveRecord::Schema.define(version: 20170814171924) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170813091208) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
