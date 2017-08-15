c2451ee13a30e08ec5f2bec2ea4bf9e967df0ba0
Arvind.H <arvind.hasti@gmail.com>
2017-08-15 21:15:29 +0530

    adjusted sign_controller but remember_token checkbox rendering errors

--- app/helpers/sign_helper.rb
+++ app/helpers/sign_helper.rb
@@ -1,2 +1,5 @@
 module SignHelper
+  
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 	get 'sign/in_new'
 	get 'sign/in_create'=>'sign#in_new'
 	post 'sign/in_create'
-	get 'sign/out'
+	get 'sign/destroy'
 	get 'sign/up_confirm'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
