8bec91341965a7b576c4c602a4d144b1a3145e54
Arvind.H <arvind.hasti@gmail.com>
2017-08-23 23:42:44 +0530

    disabled sprockets

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,47 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.fromLeft',{
+   duration: 1300,
+   origin:'left',
+   easing:'cubic-bezier(1,-0.03, 0.15, 1.23)',
+   distance: '100px'
+});
+sr.reveal('.fromRight',{
+   duration: 1300,
+   origin:'right',
+   easing:'cubic-bezier(1,-0.03, 0.15, 1.23)',
+   distance: '100px'
+});
+sr.reveal('.item',{
+   duration: 1300,
+   easing:'cubic-bezier(1,-0.03, 0.15, 1.23)',
+   origin: 'bottom',
+   scale: 0.001
+
+});
+sr.reveal('.logo',{
+   duration: 1000,
+   origin:'bottom',
+   scale:0.0001,
+   easing:'cubic-bezier(1,-0.03, 0, 2.31)',
+   delay:500
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+    $(".logo").hover3d({
+      selector: ".logo__card",
+      shine: true,
+      sensitivity: 20,
+   });
+   $('.btn-floating').click(function(e) {
+      var link = $(this).attr('href');
+      //console.log($(link).offset().top);
+
+    $('html, body').animate({
+      scrollTop: $(link).offset().top
+   }, 900);
+   e.preventDefault();
+  });
+
+});
