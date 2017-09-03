b76085b4a40b065cec0f2abd9ef4abacfead8640
Arvind.H <arvind.hasti@gmail.com>
2017-09-03 18:59:15 +0530

    Modified submit button and Added LogOut Link

--- app/helpers/sign_helper.rb
+++ app/helpers/sign_helper.rb
@@ -1,2 +1,29 @@
 module SignHelper
+
+ # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+
+	# Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+   # Returns the user corresponding to the remember token cookie.
+  def current_user
+    if (user_id = session[:user_id])	#assign session-user if current_user is nil.
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])	#else assign it to cookie user.
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+	
 end
